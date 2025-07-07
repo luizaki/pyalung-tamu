912f684da45ad066bcd208b51ebb1cb712d2dbff
luizaki <francinelouise.sanchez@gmail.com>
2025-07-07 19:11:48 +0800

    feat: add basic content box for leaderboard and progress

--- lib/views/leaderboard.dart
+++ lib/views/leaderboard.dart
@@ -1,39 +1,41 @@
 import 'package:flutter/material.dart';
 
-import 'home.dart';
-import 'progress.dart';
-
 class LeaderboardPage extends StatelessWidget {
   const LeaderboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.leaderboard),
-            label: 'Leaderboard',
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.grey[100],
+        child: Center(
+          // Box area
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 3 / 5,
+            child: const Card(
+              elevation: 5,
+              margin: EdgeInsets.all(32),
+              child: Padding(
+                padding: EdgeInsets.all(24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Title
+                    Text(
+                      'Leaderboard',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.timeline),
-            label: 'Progress',
-          ),
-        ],
-        onTap: (index) {
-          if (index == 0) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const HomePage()),
-            );
-          } else if (index == 2) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ProgressPage()),
-            );
-          }
-        },
+        ),
       ),
     );
   }
