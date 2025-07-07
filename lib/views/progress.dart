012ada121fc16c494867b350597dc21b2119c3ab
luizaki <francinelouise.sanchez@gmail.com>
2025-07-07 19:11:48 +0800

    feat: add basic content box for leaderboard and progress

--- lib/views/progress.dart
+++ lib/views/progress.dart
@@ -5,6 +5,38 @@ class ProgressPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.grey[100],
+        child: Center(
+          // Box area
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 3 / 5,
+            child: const Card(
+              elevation: 5,
+              margin: EdgeInsets.all(32),
+              child: Padding(
+                padding: EdgeInsets.all(24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    // Title
+                    Text(
+                      'Progress',
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
