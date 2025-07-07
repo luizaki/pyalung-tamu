65b349336356e5ce67e31098932af97b8ac45f06
luizaki <francinelouise.sanchez@gmail.com>
2025-07-07 18:48:14 +0800

    feat: add navigation bar to leaderboard and progress

--- lib/views/leaderboard.dart
+++ lib/views/leaderboard.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 import 'home.dart';
+import 'progress.dart';
 
 class LeaderboardPage extends StatelessWidget {
   const LeaderboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: 'Leaderboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timeline),
+            label: 'Progress',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          } else if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ProgressPage()),
+            );
+          }
+        },
+      ),
+    );
   }
 }
--- lib/views/progress.dart
+++ lib/views/progress.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 import './home.dart';
+import './leaderboard.dart';
 
 class ProgressPage extends StatelessWidget {
   const ProgressPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: 'Leaderboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timeline),
+            label: 'Progress',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomePage()),
+            );
+          } else if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const LeaderboardPage()),
+            );
+          }
+        },
+      ),
+    );
   }
 }
