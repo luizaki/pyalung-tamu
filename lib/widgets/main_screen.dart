edad1712442e07aae5f5f4a69b5e4c3761c3edff
luizaki <francinelouise.sanchez@gmail.com>
2025-07-10 19:42:59 +0800

    fix: fix alignment of main screen widget

--- lib/widgets/main_screen.dart
+++ lib/widgets/main_screen.dart
@@ -21,32 +21,35 @@ class MainScreen extends StatelessWidget {
           ),
         ),
 
-        Center(
-          child: SizedBox(
-              width: MediaQuery.of(context).size.width * 3 / 5,
-              child: Container(
-                  margin: const EdgeInsets.all(32),
-                  decoration: BoxDecoration(
-                    color: const Color(0xF9DD9A00),
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(
-                      color: const Color(0xAD572100),
-                      width: 10,
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color(0xAD572100).withOpacity(0.2),
-                        blurRadius: 5,
-                        offset: const Offset(0, 2),
+        Container(
+          padding: const EdgeInsets.only(bottom: 80),
+          child: Center(
+            child: SizedBox(
+                width: MediaQuery.of(context).size.width * 3 / 5,
+                child: Container(
+                    margin: const EdgeInsets.all(32),
+                    decoration: BoxDecoration(
+                      color: const Color(0xF9DD9A00),
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: const Color(0xAD572100),
+                        width: 10,
                       ),
-                    ],
-                  ),
-                  child: Padding(
-                      padding: const EdgeInsets.all(24),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: children,
-                      )))),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xAD572100).withOpacity(0.2),
+                          blurRadius: 5,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: children,
+                        )))),
+          ),
         ),
       ],
     );
