be391ed96bf733bc14c55e4777443e71916a8d7d
luizaki <francinelouise.sanchez@gmail.com>
2025-07-17 14:35:40 +0800

    refactor: fix main_screen constructor

--- lib/widgets/main_screen.dart
+++ lib/widgets/main_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   final List<Widget> children;
 
-  MainScreen({required this.children});
+  const MainScreen({super.key, required this.children});
 
   @override
   Widget build(BuildContext context) {
