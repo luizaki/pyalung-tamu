8645c4886829336ea18b0c11dedcca721bb6e206
luizaki <francinelouise.sanchez@gmail.com>
2025-08-05 02:41:17 +0800

    feat: link tugak game screen to start screen

--- lib/games/mitutuglung/start_screen.dart
+++ lib/games/mitutuglung/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../widgets/start_screen.dart';
+import '../../views/home.dart';
 
 class MitutuglungStartScreen extends StatelessWidget {
   const MitutuglungStartScreen({super.key});
@@ -7,12 +8,14 @@ class MitutuglungStartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const StartScreen(
-        color1: Color(0xFFFF5E2D),
-        color2: Color(0xFFFF7E47),
-        color3: Color(0xFFFFAF55),
-        color4: Color(0xFFFFD859),
-        gameTitle: 'Mitutuglung',
-        instructions:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+      color1: Color(0xFFFF5E2D),
+      color2: Color(0xFFFF7E47),
+      color3: Color(0xFFFFAF55),
+      color4: Color(0xFFFFD859),
+      gameTitle: 'Mitutuglung',
+      instructions:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      gameScreen: HomePage(), // placeholder
+    );
   }
 }
--- lib/games/siglulung_bangka/start_screen.dart
+++ lib/games/siglulung_bangka/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../widgets/start_screen.dart';
+import '../../views/home.dart';
 
 class BangkaStartScreen extends StatelessWidget {
   const BangkaStartScreen({super.key});
@@ -7,12 +8,14 @@ class BangkaStartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const StartScreen(
-        color1: Color(0xFF070635),
-        color2: Color(0xFF14085C),
-        color3: Color(0xFF0A0C80),
-        color4: Color(0xFF2312C0),
-        gameTitle: 'Siglulung Bangka',
-        instructions:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+      color1: Color(0xFF070635),
+      color2: Color(0xFF14085C),
+      color3: Color(0xFF0A0C80),
+      color4: Color(0xFF2312C0),
+      gameTitle: 'Siglulung Bangka',
+      instructions:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      gameScreen: HomePage(), // placeholder
+    );
   }
 }
