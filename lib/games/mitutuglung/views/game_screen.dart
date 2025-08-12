cd702b7e049ca9f726d99e326b34d3eccd584aa7
luizaki <francinelouise.sanchez@gmail.com>
2025-08-12 22:16:25 +0800

    feat: create mitutuglung ui

---
+++ lib/games/mitutuglung/views/game_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/widgets/base_game_screen.dart';
+import '../controllers/game_controller.dart';
+import '../widgets/card_widget.dart';
+
+class MitutuglungGameScreen extends BaseGameScreen<MitutuglungGameController> {
+  const MitutuglungGameScreen({super.key});
+
+  @override
+  MitutuglungGameScreenState createState() => MitutuglungGameScreenState();
+}
+
+class MitutuglungGameScreenState extends BaseGameScreenState<
+    MitutuglungGameController, MitutuglungGameScreen> {
+  @override
+  MitutuglungGameController createController() {
+    return MitutuglungGameController();
+  }
+
+  @override
+  void setupController() {}
+
+  @override
+  void onControllerUpdate() {}
+
+  @override
+  void disposeGameSpecific() {}
+
+  @override
+  List<Widget> buildGameSpecificWidgets() {
+    return [
+      _buildCardsGrid(),
+    ];
+  }
+
+  Widget _buildCardsGrid() {
+    final cardsGrid = controller.getCardsGrid();
+
+    return Positioned(
+      top: 220,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: cardsGrid.map((row) => _buildCardRow(row)).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCardRow(List<dynamic> cards) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: cards
+          .map((card) => CardWidget(
+                card: card,
+                onTap: () => controller.onCardTapped(card),
+              ))
+          .toList(),
+    );
+  }
+}
---
+++ lib/games/mitutuglung/widgets/card_widget.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import '../models/card.dart';
+
+class CardWidget extends StatelessWidget {
+  final MitutuglungCard card;
+  final VoidCallback onTap;
+  final double width;
+  final double height;
+
+  const CardWidget({
+    super.key,
+    required this.card,
+    required this.onTap,
+    this.width = 75.0,
+    this.height = 90.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        width: width,
+        height: height,
+        margin: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: _getCardColor(),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: _getBorderColor(),
+            width: 3,
+          ),
+        ),
+        child: _buildCardContent(),
+      ),
+    );
+  }
+
+  Color _getCardColor() {
+    if (card.isHidden) {
+      return const Color(0xFF2BB495);
+    } else if (card.isMatched) {
+      return const Color(0xFF965D08);
+    } else {
+      return const Color(0xF9DD9A00);
+    }
+  }
+
+  Color _getBorderColor() {
+    return const Color(0xAD572100);
+  }
+
+  Widget _buildCardContent() {
+    if (card.isHidden) {
+      return const Center(
+        child: Icon(
+          Icons.help_outline,
+          size: 40,
+          color: Colors.brown,
+        ),
+      );
+    }
+
+    if (card.isWord) {
+      return Center(
+        child: Text(card.content,
+            style: const TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.brown),
+            textAlign: TextAlign.center),
+      );
+    } else {
+      return Center(
+        child: Image.network(
+          card.content,
+          fit: BoxFit.cover,
+          width: 30,
+          height: 30,
+        ),
+      );
+    }
+  }
+}
